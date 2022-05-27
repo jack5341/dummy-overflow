09b65b90f3bce0eae5ab1ee58ff956ee24e6eace
Nedim Akar <nedim.akar53411@gmail.com>
2022-05-27 15:48:02 +0200

    feat: db connection is stabilized

--- src/configurations/configurations.ts
+++ src/configurations/configurations.ts
@@ -1,7 +1,11 @@
+import { config } from 'dotenv';
+
+config();
+
 export const DATABASE = {
-  HOST: 'localhost' || process.env.DATABASE_HOST,
-  PORT: '5432' || process.env.DATABASE_PORT,
-  USER: 'postgres' || process.env.DATABASE_USER,
-  PASSWORD: 'postgres' || process.env.DATABASE_PASSWORD,
-  DATABASE: 'dummy-overflow' || process.env.DATABASE_DATABASE,
+  HOST: process.env.POSTGRES_HOST || 'localhost',
+  PORT: process.env.POSTGRES_PORT || '5432',
+  USER: process.env.POSTGRES_USER || 'postgres',
+  PASSWORD: process.env.POSTGRES_PASSWORD || 'postgres',
+  DATABASE: process.env.POSTGRES_DATABASE || 'dummy-overflow',
 };
